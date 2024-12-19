37bbfa4331df9c9a1e73c21e377111f76d1d7ad2
Maksim Bondar <lestatuk@gmail.com>
2024-12-19 17:07:42 +0700

    Обновил README.md и urls поправил

--- robots/urls.py
+++ robots/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import create_robot, create_robot_report
 
 urlpatterns = [
-    path('api/create_robot/', create_robot),
+    path('create_robot/', create_robot),
     path('create_report/', create_robot_report)
 ]
